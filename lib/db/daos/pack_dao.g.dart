ae871e99e3113ac58fd54f387196c2e2544fa137
tilde <tilde@synapseapp.dev>
2026-08-30 15:52:41 +0800

    fix(deps): upgrade drift and drift_dev to 2.31.0 for compatibility with modern analyzer 10.2 AST parser

--- lib/db/daos/pack_dao.g.dart
+++ lib/db/daos/pack_dao.g.dart
@@ -9,4 +9,20 @@ mixin _$PackDaoMixin on DatabaseAccessor<AppDatabase> {
   $TagsTable get tags => attachedDatabase.tags;
   $QuestionTagsTable get questionTags => attachedDatabase.questionTags;
   $UserProgressTable get userProgress => attachedDatabase.userProgress;
+  PackDaoManager get managers => PackDaoManager(this);
+}
+
+class PackDaoManager {
+  final _$PackDaoMixin _db;
+  PackDaoManager(this._db);
+  $$PacksTableTableManager get packs =>
+      $$PacksTableTableManager(_db.attachedDatabase, _db.packs);
+  $$QuestionsTableTableManager get questions =>
+      $$QuestionsTableTableManager(_db.attachedDatabase, _db.questions);
+  $$TagsTableTableManager get tags =>
+      $$TagsTableTableManager(_db.attachedDatabase, _db.tags);
+  $$QuestionTagsTableTableManager get questionTags =>
+      $$QuestionTagsTableTableManager(_db.attachedDatabase, _db.questionTags);
+  $$UserProgressTableTableManager get userProgress =>
+      $$UserProgressTableTableManager(_db.attachedDatabase, _db.userProgress);
 }
--- lib/db/daos/progress_dao.g.dart
+++ lib/db/daos/progress_dao.g.dart
@@ -6,4 +6,14 @@ part of 'progress_dao.dart';
 mixin _$ProgressDaoMixin on DatabaseAccessor<AppDatabase> {
   $UserProgressTable get userProgress => attachedDatabase.userProgress;
   $QuestionsTable get questions => attachedDatabase.questions;
+  ProgressDaoManager get managers => ProgressDaoManager(this);
+}
+
+class ProgressDaoManager {
+  final _$ProgressDaoMixin _db;
+  ProgressDaoManager(this._db);
+  $$UserProgressTableTableManager get userProgress =>
+      $$UserProgressTableTableManager(_db.attachedDatabase, _db.userProgress);
+  $$QuestionsTableTableManager get questions =>
+      $$QuestionsTableTableManager(_db.attachedDatabase, _db.questions);
 }
